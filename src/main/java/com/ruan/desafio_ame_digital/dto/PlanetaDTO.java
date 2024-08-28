aea1328342d999e2859277f860368d6a78507055
Ruan <camposcosta.rc@gmail.com>
2024-08-28 17:44:37 -0300

    Adding other methods

--- src/main/java/com/ruan/desafio_ame_digital/dto/PlanetaDTO.java
+++ src/main/java/com/ruan/desafio_ame_digital/dto/PlanetaDTO.java
@@ -4,22 +4,31 @@ import com.ruan.desafio_ame_digital.model.Planeta;
 
 public class PlanetaDTO {
 
+    private Long id;
     private String nome;
     private String clima;
+    private String terreno;
     private Integer numeroAparicoes;
 
-    public PlanetaDTO(String nome, String clima){
+    public PlanetaDTO(String nome, String clima, String terreno,  Integer numeroAparicoes){
         this.nome = nome;
         this.clima = clima;
+        this.numeroAparicoes = numeroAparicoes;
     }
 
     public PlanetaDTO() {
     }
 
     public PlanetaDTO(Planeta planeta){
+        this.id = planeta.getId();
         this.nome = planeta.getNome();
         this.clima = planeta.getClima();
+        this.terreno = planeta.getTerreno();
         this.numeroAparicoes = planeta.getNumeroAparicoes();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNome() {
@@ -36,5 +45,21 @@ public class PlanetaDTO {
 
     public void setClima(String clima) {
         this.clima = clima;
+    }
+
+    public String getTerreno() {
+        return terreno;
+    }
+
+    public void setTerreno(String terreno) {
+        this.terreno = terreno;
+    }
+
+    public Integer getNumeroAparicoes() {
+        return numeroAparicoes;
+    }
+
+    public void setNumeroAparicoes(Integer numeroAparicoes) {
+        this.numeroAparicoes = numeroAparicoes;
     }
 }
